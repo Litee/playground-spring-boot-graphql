b7acdd522569f2e269f0d8fc968dc1661fae21c1
Litee <lipatkin@gmail.com>
2017-12-30 23:37:48 +0300

    Added one more test, minor refactoring

--- src/test/java/ru/lipatkin/playground/spring/boot/graphql/QueryTest.java
+++ src/test/java/ru/lipatkin/playground/spring/boot/graphql/QueryTest.java
@@ -22,14 +22,27 @@ public class QueryTest {
     @Test
     @SneakyThrows
     public void testLoadingBooks() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<String> entity = new HttpEntity<>("{\"query\":\"{books{id, title}}\"}", headers);
-
-        ResponseEntity<String> responseEntity = restTemplate.postForEntity("/graphql", entity, String.class);
+        ResponseEntity<String> responseEntity = postGraphql("{books{id, title}}");
 
         assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
         JSONAssert.assertEquals("{\"data\":{\"books\":[{\"id\":0,\"title\":\"Foundation\"},{\"id\":1,\"title\":\"I, robot\"},{\"id\":2,\"title\":\"Star Troopers\"},{\"id\":3,\"title\":\"Double star\"}]}}", responseEntity.getBody(), false);
+    }
+
+    @Test
+    @SneakyThrows
+    public void testGettingSingleBook() {
+        ResponseEntity<String> responseEntity = postGraphql("{book(id:1){id, title}}");
+
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        JSONAssert.assertEquals("{\"data\":{\"book\":{\"id\":1,\"title\":\"I, robot\"}}}", responseEntity.getBody(), false);
+    }
+
+    private ResponseEntity<String> postGraphql(String query) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> entity = new HttpEntity<>("{\"query\":\"" + query + "\"}", headers);
+
+        return restTemplate.postForEntity("/graphql", entity, String.class);
     }
 
 }
